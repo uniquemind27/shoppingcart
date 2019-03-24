048154ba125bb9a524ccd1375a7a60aa2c1cc4f3
Nagharajan Balraj <study@uniquemind.me>
2019-03-25 02:23:36 +1100

    Spell Error - Custom Exception - User

--- user/src/main/java/com/uniquemind/user/exception/UserAlreadyFoundException.java
+++ user/src/main/java/com/uniquemind/user/exception/UserAlreadyFoundException.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * @author Nag
  *
  */
-@ResponseStatus(code=HttpStatus.BAD_REQUEST, reason="User ALready Exists")
+@ResponseStatus(code=HttpStatus.BAD_REQUEST, reason="User Already Exists")
 public class UserAlreadyFoundException extends Exception{
 
 	private static final long serialVersionUID = 1L;
