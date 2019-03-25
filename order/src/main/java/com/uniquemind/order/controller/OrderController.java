441b075fc79520105c53630dbbd2c05111b9240f
Nagharajan Balraj <study@uniquemind.me>
2019-03-25 18:47:30 +1100

    Order - Controller

--- order/src/main/java/com/uniquemind/order/controller/OrderController.java
+++ order/src/main/java/com/uniquemind/order/controller/OrderController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,10 +18,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.uniquemind.order.exception.OrderFailureException;
 import com.uniquemind.order.exception.OrderNotFoundException;
+import com.uniquemind.order.exception.ProductNotFoundException;
+import com.uniquemind.order.exception.UserNotFoundException;
 import com.uniquemind.order.model.Order;
+import com.uniquemind.order.model.Product;
+import com.uniquemind.order.model.User;
 import com.uniquemind.order.service.OrderService;
 
 /**
@@ -38,6 +44,9 @@ public class OrderController {
 	
 	private ResponseEntity<Order> responseEntity;
 	
+	@Autowired
+	RestTemplate restTemplate;
+	
 	public OrderController() {
 		super();
 	}
@@ -49,8 +58,30 @@ public class OrderController {
 	}
 
 	@PostMapping(path="")
-	public ResponseEntity<Order> placeOrder(@RequestBody Order order) throws OrderFailureException{
+	public ResponseEntity<Order> placeOrder(@RequestBody Order order) throws OrderFailureException, UserNotFoundException, ProductNotFoundException{
 		logger.info("Placing an Order");
+		
+//		HttpHeaders headers = new HttpHeaders();
+//	    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//	    HttpEntity<Product> entity = new HttpEntity<Product>(,headers);
+	      
+		
+		logger.info("Calling the product service to check on the productid "+order.getProductId());
+		String  productURL = "http://localhost:9070/api/v1/products/"+order.getProductId();
+	    ResponseEntity<Product> productEntity = restTemplate.exchange(productURL, HttpMethod.GET, null, Product.class);
+	    logger.info("product id check successful "+productEntity.getStatusCodeValue());
+	    if(!productEntity.getStatusCode().is2xxSuccessful()){
+	    	throw new ProductNotFoundException();
+	    }
+	    
+		logger.info("Calling the user service to check on the userid "+order.getUserId());
+		String userURL = "http://localhost:9080/api/v1/users/"+order.getUserId();
+	    ResponseEntity<User> userEntity = restTemplate.exchange(userURL, HttpMethod.GET, null, User.class);
+	    logger.info("user id check successful "+userEntity.getStatusCodeValue());
+	    if(!userEntity.getStatusCode().is2xxSuccessful()){
+	    	throw new UserNotFoundException();
+	    }
+	    
 		try {
 			orderService.saveOrder(order);
 			responseEntity = new ResponseEntity<Order>(order, HttpStatus.CREATED);
